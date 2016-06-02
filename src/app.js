787973ecd9a7cd9bd868a3f408be1c34f8928746
TejaswiR <tejaswi.ramisetty@gmail.com>
2016-06-02 15:08:18 +0530

    added js file,Extracted data in homepage

---
+++ src/app.js
@@ -0,0 +1,29 @@
+var angularApp = angular.module('Football-App',['ngResource','ngRoute'])
+
+angularApp.config(function($routeProvider,$httpProvider) {
+  $httpProvider.defaults.headers.get = { 'X-Auth-Token' : '5d2b18209c4f44c5a61fa93089c6cdfb'  }
+  $routeProvider
+
+  .when('/',
+  {
+    templateUrl: 'src/main.html',
+    contoller: 'MainController',
+    cotrollerAs : 'vm'
+  })
+
+  .when ('/footballdetails/:uniqId',
+  {
+    templateUrl: 'src/footballdetails.html',
+    controller: 'DetailsController'
+  })
+});
+
+
+angularApp.controller("MainController",['$resource',
+function($resource){
+   var vm=this;
+   var football = $resource('http://api.football-data.org/v1/soccerseasons/');
+   vm.footResponse = football.query();
+   console.log(vm.footResponse);
+
+}]);
